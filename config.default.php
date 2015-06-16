585f47935836f943673d2abe758c71da0c861585
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2015-06-16 11:21:14 +0200

    Qual: Include is not a function but a language construct

--- config.default.php
+++ config.default.php
@@ -1,15 +1,15 @@
 <?php
 	
 	if(is_file('../main.inc.php'))$dir = '../';
-	else  if(is_file('../../../main.inc.php'))$dir = '../../../';
+	else if(is_file('../../../main.inc.php'))$dir = '../../../';
 	else $dir = '../../';
 
 
 	if(!defined('INC_FROM_DOLIBARR') && defined('INC_FROM_CRON_SCRIPT')) {
-		include($dir."master.inc.php");
+		include $dir.'master.inc.php';
 	}
 	elseif(!defined('INC_FROM_DOLIBARR')) {
-		include($dir."main.inc.php");
+		include $dir.'main.inc.php';
 	} else {
 		global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
 	}
@@ -26,6 +26,4 @@
 		print $langs->trans('AbricotNotFound'). ' : <a href="http://wiki.atm-consulting.fr/index.php/Accueil#Module_Abricot" target="_blank">Abricot</a>';
 		exit;
 	}
-
-	
 
